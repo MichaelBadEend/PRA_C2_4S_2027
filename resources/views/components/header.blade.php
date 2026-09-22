20f2f2489ececbb7271fa943810ed995f05e261c
itzhorror <itzhorror69@gmail.com>
2026-09-22 12:03:24 +0200

    test

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,6 +4,10 @@
         <a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">
             <h1>{{ __('misc.homepage_title') }}</h1>
         </a>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
         {{ $introduction_text ?? '' }}
     </div>
 </div>
