32e2c5c045aa9062c290a94d46ecc709088a0794
MichaelBadEend <D317526@edu.curio.nl>
2026-09-22 12:18:07 +0200

    De footer onderin gezet en verbeterd

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,39 +9,29 @@
 
 <div class="container">
     <div class="row">
-
         <div class="col-md-8">
             <x-header/>
-
             <ul class="breadcrumb">
                 <li><a href="/" title="{{ __('misc.home_alt') }}"
                        alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a></li>
                 {{ $breadcrumb ?? '' }}
             </ul>
-
             @if ( isset($_GET['q']) )
                 <x-search_results/>
             @else
                 {{ $slot }}
             @endif
-
             <ul class="breadcrumb">
                 <li>
 					<a href="/" title="{{ __('misc.home_alt') }}" alt="{{ __('misc.home_alt') }}">{{ __('misc.home') }}</a>
 				</li>
                 {{ $breadcrumb ?? '' }}
             </ul>
-
         </div>
-
-        <div class="row">
-            <x-footer/>
-        </div>
-
     </div>
 
-
 </div>
+<x-footer/>
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
